ba4c17c4c0a8b69da695da168ca756310ba04433
ReaganPastor <reaganelizabeth@gmail.com>
2026-02-17 13:24:55 -0500

    Repo set up (#4)
    * Set up backend
    
    * Connected Backend and Frontend

---
+++ backend/src/main/java/com/team20ces/moviebooking/HelloController.java
@@ -0,0 +1,16 @@
+package com.team20ces.moviebooking;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000") // Allow React frontend to access
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Spring Boot!";
+    }
+
+}
