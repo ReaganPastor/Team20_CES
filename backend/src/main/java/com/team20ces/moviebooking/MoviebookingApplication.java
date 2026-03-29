ade45942e5275f8815450a83d113cec092db2df2
Reagan Pastor <reaganelizabeth@gmail.com>
2026-03-29 13:10:24 -0400

    Update MovieBookingApp

--- backend/src/main/java/com/team20ces/moviebooking/MoviebookingApplication.java
+++ backend/src/main/java/com/team20ces/moviebooking/MoviebookingApplication.java
@@ -3,7 +3,10 @@ package com.team20ces.moviebooking;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.scheduling.annotation.EnableAsync;
+
 @SpringBootApplication
+@EnableAsync
 public class MoviebookingApplication {
     public static void main(String[] args) {
         SpringApplication.run(MoviebookingApplication.class, args);
